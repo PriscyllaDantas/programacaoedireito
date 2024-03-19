2c4db78eca674f3e2501131359dc24ba72d5645e
PriscyllaDantas <159178635+PriscyllaDantas@users.noreply.github.com>
2024-03-19 15:05:46 -0300

    Add files via upload

---
+++ Semana6.py
@@ -0,0 +1,21 @@
+numero = 10
+nome = "João"
+booleano = True
+
+a = True
+b = False
+resultado1 = a and b
+resultado2 = a or b
+resultado3 = not a
+a = 10
+b = 5
+c = 20
+soma = a + b
+subtração = a - b
+multiplicação = a * b
+divisao = a / b
+# a ordem de precedencia dos operadores logicos é not; and; or
+# parênteses definem a ordem de avaliação dos operadores lógicos
+
+resultado4 = a > 5 and b < 10 or c == 20
+resultado5 = (a > 5) and ((b < 10) or (c==20))
